c7440ffd6be1903b71cd65d27e1281f57d6c0a0e
Corey Zhang <coreyjzhang@gmail.com>
2021-02-01 17:03:59 -0600

    fix margin

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(600, 400, Phaser.AUTO);
+var game = new Phaser.Game(1500, 1000, Phaser.AUTO);
 game.state.add('state1', demo.state1);
 game.state.add('state2', demo.state2);
 game.state.add('state3', demo.state3);
--- state0.js
+++ state0.js
@@ -7,6 +7,8 @@ demo.state0.prototype = {
         console.log('state0');  
         
         addChangeStateEventListeners();
+        
+        game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 
     },
     update: function(){}
